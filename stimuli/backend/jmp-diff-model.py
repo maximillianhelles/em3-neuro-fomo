6b89efb78511871a8f1c480f2c8b82e1383ca06d
maximillianhelles <maximillian.helles@gmail.com>
2026-03-15 10:03:09 +0100

    Full functionality of jmp-diff-model achieved

--- stimuli/backend/jmp-diff-model.py
+++ stimuli/backend/jmp-diff-model.py
@@ -1,21 +1,42 @@
 import yaml
 import numpy as np
 import matplotlib.pyplot as plt
-import json
+import random
 
 params_path = "../../config/params.yaml"
 
 with open(params_path, "r") as f:
     params = yaml.safe_load(f)
 
-s0 = params["gbm"]["init_value"]
-drift = params["gbm"]["mu"]
-volatility = params["gbm"]["sigma"]
-periods = params["gbm"]["periods"]
+def get_prices(
+        init_value=params["gbm"]["init_value"], drift=params["gbm"]["mu"], 
+        volatility=params["gbm"]["sigma"], periods=params["gbm"]["periods"],
+        jump=params["gbm"]["mu_jump"], std_jump=params["gbm"]["sigma_jump"]):
+    
+    # GBM calculation
+    shocks = np.random.normal(drift, volatility, periods)
+    gbm_values = init_value * np.exp(np.cumsum(shocks))
+    gbm_values = np.insert(gbm_values, 0, init_value).tolist()
 
-shocks = np.random.normal(drift, volatility, periods)
-values = s0 * np.exp(np.cumsum(shocks))
-values = np.insert(values, 0, s0).tolist()
+    # Intraday jump
+    jump_point = random.randint(int(periods/10), periods - int(periods/10)) # ensures margin for start and end
+    value = gbm_values[jump_point]
+    pct_jump = np.random.normal(jump, std_jump)
+    direction = random.choice([-1,1])
+    value *= 1+direction*pct_jump
 
-with open("values.json", "w") as f:
-    json.dump(values, f)
+    # Update GBM values 
+    pre_values = gbm_values[:jump_point]
+    remaining_shocks = shocks[jump_point+1:]
+    pre_values.append(value)
+    post_values = value * np.exp(np.cumsum(remaining_shocks))
+    total_values = pre_values + post_values.tolist()
+
+    return total_values, pct_jump
+
+test, jump = get_prices()
+print(f"--- VALUES --- \n {test}")
+print(f"\n --- PERCENTAGE JUMP --- \n {round(jump*100, 2)}%")
+
+plt.plot(test)
+#plt.show()
