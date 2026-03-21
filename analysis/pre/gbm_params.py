40f62591203e8e3edded3d899aeed26edbc605ca
maximillianhelles <maximillian.helles@gmail.com>
2026-03-21 22:42:14 +0100

    Changed data source so modified code to handle csv

--- analysis/pre/gbm_params.py
+++ analysis/pre/gbm_params.py
@@ -1,10 +1,12 @@
-import yfinance as yf
 import numpy as np
 import yaml
+import csv
 import os
+import pandas as pd
 
-def estimate_gbm_params(df):
-    log_rets = np.log(df / df.shift(1))
+def estimate_gbm_params(prices, timestamps):
+    s = pd.Series(prices, index=pd.to_datetime(timestamps))
+    log_rets = np.log(s / s.shift(1))
     grouped = log_rets.groupby(log_rets.index.date)
 
     results = []
@@ -27,29 +29,22 @@ def estimate_gbm_params(df):
 ######################################################################
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
+data_path = os.path.join(base_dir, "../../data/financial_data/sp500_1m.csv")
 yaml_path = os.path.join(base_dir, "../../config/params.yaml")
+
+with open(data_path, "r") as f:
+    snp_1m = list(csv.DictReader(f))
+
+closes = [float(day["close"]) for day in snp_1m]
+timestamps = [day["datetime"] for day in snp_1m]
+
+mu, sigma = estimate_gbm_params(closes, timestamps)
 
 with open(yaml_path,"r") as f:
     params = yaml.safe_load(f)
 
-ticker = params["jump_gbm"]["ticker"]
-period = "7d"
-interval = "1m"
-df = (yf.download(ticker, period=period, interval=interval))["Close"]
-df = df.squeeze()
-
-if df.empty:
-    raise ValueError(f"No data returned for ticker: {ticker}. Double-check input parameters")
-
-mu_empirical, sigma_empirical = estimate_gbm_params(df)
-
-params["jump_gbm"]["mu"] = round(float(mu_empirical),6)
-params["jump_gbm"]["sigma"] = round(float(sigma_empirical),6)
-
-if interval == "1m":
-    params["jump_gbm"]["periods"] = 60*8
-elif interval == "5m":
-    params["jump_gbm"]["periods"] = 12*8
+params["jdm"]["mu"] = float(mu)
+params["jdm"]["sigma"] = float(sigma)
 
 with open(yaml_path,"w") as f:
     yaml.dump(params, f)
