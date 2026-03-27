c344714a6c22a72bcc55057a99fd67319975cdae
maximillianhelles <maximillian.helles@gmail.com>
2026-03-27 18:18:01 +0100

    Finalized block logic

--- experiment/blocks.py
+++ experiment/blocks.py
@@ -16,23 +16,37 @@ config_path = os.path.join(base_dir, "../config/params.yaml")
 with open(config_path,"r") as f:
     params = yaml.safe_load(f)
 
-def run_block(block_id, fullscr=False):
-    combos = [("ASSET", 1), ("ASSET", -1), ("CASH", 1), ("CASH", -1)]
-    all_trials = combos * params["exp"]["trials_per_condition"]
-    random.shuffle(all_trials)
-    trials, directions = zip(*all_trials)
-    tickers = random.sample(params["exp"]["tickers"], len(all_trials))
-
+def run_block(subject_id, block_id, fullscr=False):
+    # Determine intial capital
     capital_map = {"control": 0, "low": 50, "high": 100}
     if block_id not in capital_map:
         raise ValueError(f"{block_id} is an invalid block_id")
     capital = capital_map[block_id]
 
+    # Create new dataset
+    csv_path = os.path.join(base_dir, f"../data/behavioral_data/{block_id}/{subject_id}_results_block.csv")
+    
+    fieldnames = ["participant_id", "block_id", "trial_num", "ticker",
+                "position", "direction", "chart_values", "action_taken", "action_value",
+                "final_value", "valence", "arousal", "regret"]
+
+    with open(csv_path, "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+
+    # Randomize trials
+    combos = [("ASSET", 1), ("ASSET", -1), ("CASH", 1), ("CASH", -1)]
+    all_trials = combos * params["exp"]["trials_per_condition"]
+    random.shuffle(all_trials)
+    trials, directions = zip(*all_trials)
+    tickers = random.sample(params["exp"]["tickers"], len(all_trials))
+    trial_nums = range(1, len(all_trials)+1)
+
     trigger = get_trigger_sender()
     block = ExpInterface(fullscr)
     trigger.send(TriggerCode.BLOCK_START)
 
-    for trial, ticker, direction in zip(trials,tickers, directions):
+    for trial, ticker, direction, trial_num in zip(trials, tickers, directions, trial_nums):
         if trial == "ASSET":
             trigger.send(TriggerCode.ASSET)
         else:
@@ -44,18 +58,32 @@ def run_block(block_id, fullscr=False):
         # Trial begins
         values, jump, jump_point = jdm(init_value=capital, direction=direction)
         trigger.send(TriggerCode.TRIAL_START)
-        block.chart_phase(values, trial, jump, jump_point, trigger)
-
+        action_taken, action_value = block.chart_phase(values, trial, jump, jump_point, trigger)
         # SAM-rating
         trigger.send(TriggerCode.SAM_RATING)
         responses = block.sam_rating()
 
-        # Log trial to CSV logic
+        # Append to behavioral data to CSV
+        with open(csv_path, "a", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writerow({
+                "participant_id": subject_id,
+                "block_id": block_id,
+                "trial_num": trial_num,
+                "ticker": ticker,
+                "position": trial,
+                "direction": direction,
+                "chart_values": [round(v, 4) for v in values],
+                "action_taken": action_taken,
+                "action_value": action_value,
+                "final_value": values[-1],
+                "valence": responses["valence"],
+                "arousal": responses["arousal"],
+                "regret": responses["regret"],
+            })
 
-
+        # Fix Cross
         trigger.send(TriggerCode.TRIAL_END)
         block.fix_cross()
     
     trigger.send(TriggerCode.BLOCK_END)
-
-run_block("low")
