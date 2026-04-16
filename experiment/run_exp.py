fc1df1ba578645da34328c1fbc742027a5e9096f
maximillianhelles <maximillian.helles@gmail.com>
2026-04-16 17:30:26 +0200

    Fixed buy-in error

--- experiment/run_exp.py
+++ experiment/run_exp.py
@@ -6,14 +6,14 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(base_dir, ".."))
 config_path = os.path.join(base_dir, "../config/params.yaml")
 with open(config_path, "r") as f:
-    params = yaml.safeload(f)
+    params = yaml.safe_load(f)
 
 from stimuli.frontend.display import ExpInterface, ExperimentAborted
 from blocks import run_block
 
 exp_interface = ExpInterface(fullscr=False)
-subject_id = "001"
-blocks = params["exp"]["blocks"]
+subject_id = "002"
+blocks = ["high"]
 
 try:
     for block in blocks:
