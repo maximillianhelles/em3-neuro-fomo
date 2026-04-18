20c7882644bae8f9a9683b38aa1af6a8434e28e8
maximillianhelles <maximillian.helles@gmail.com>
2026-04-18 21:24:33 +0200

    JDM now compatible with seeding

--- stimuli/backend/jmp_diff_model.py
+++ stimuli/backend/jmp_diff_model.py
@@ -1,6 +1,5 @@
 import yaml
 import numpy as np
-import random
 import os
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
@@ -13,17 +12,20 @@ def calc_jdm_values(
         init_value=100, drift=params["jdm"]["mu"], 
         volatility=params["jdm"]["sigma"], periods=params["jdm"]["periods"],
         jump=params["jdm"]["mu_jump"], std_jump=params["jdm"]["sigma_jump"],
-        direction=1):
+        direction=1, rng = None):
+    
+    if rng is None:
+        rng = np.random.default_rng()
     
     # GBM calculation
-    shocks = np.random.normal(drift - 0.5 * volatility**2, volatility, periods)
+    shocks = rng.normal(drift - 0.5*volatility**2, volatility, periods)
     gbm_values = init_value * np.exp(np.cumsum(shocks))
     gbm_values = np.insert(gbm_values, 0, init_value).tolist()
 
     # Intraday spike
-    jump_point = random.randint(int(periods/10), periods - int(periods/10)) # ensures 10% margin for start and end
+    jump_point = int(rng.integers(periods//10, periods - periods//10)) # ensures 10% margin for start and end
     value = gbm_values[jump_point]
-    pct_jump = np.random.normal(jump, std_jump)
+    pct_jump = rng.normal(jump, std_jump)
     value *= 1+direction*pct_jump
 
     # Update GBM values 
